490a7004ecac6dddaa043332cdebc1f791e8a20a
baye94 <fallcheikh893@gmail.com>
2023-01-18 23:49:39 +0000

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,32 @@
-# #!/usr/bin/env python3
-# """
-# create a web cach
-# """
-# import redis
-# import requests
-# rc = redis.Redis()
-# count = 0
+#!/usr/bin/env python3
+""" Redis Module """
+
+from functools import wraps
+import redis
+import requests
+from typing import Callable
+
+redis_ = redis.Redis()
 
 
-# def get_page(url: str) -> str:
-#     """ get a page and cach value"""
-#     rc.set(f"cached:{url}", count)
-#     resp = requests.get(url)
-#     rc.incr(f"count:{url}")
-#     rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
-#     return resp.text
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting """
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper acts as a decorator """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
 
 
-# if __name__ == "__main__":
-#     get_page('http://slowwly.robertomurray.co.uk')
+@count_requests
+def get_page(url: str) -> str:
+    """ Obtain HTML content """
+    req = requests.get(url)
+    return req.text
