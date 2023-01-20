33e25a1d64f67ef82e9ac5ba05044befd08387e7
baye94 <fallcheikh893@gmail.com>
2023-01-20 17:15:54 +0000

    fix bugs

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,49 +1,21 @@
-import requests
-import time
+#!/usr/bin/env python3
+"""
+create a web cach
+"""
 import redis
+import requests
+rc = redis.Redis()
+count = 0
 
-r = redis.Redis(host='localhost', port=6379, db=0)
 
 def get_page(url: str) -> str:
-    count_key = f"count:{url}"
-    content_key = f"content:{url}"
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
-    # Check if the URL has been accessed before
-    if r.exists(count_key):
-        r.incr(count_key)
-    else:
-        r.set(count_key, 1)
 
-    # Check if the HTML content is cached
-    if r.exists(content_key):
-        html = r.get(content_key)
-    else:
-        # Get HTML content from the URL
-        response = requests.get(url)
-        html = response.text
-        r.set(content_key, html, ex=10)
-
-    return html
-def cache(func):
-    def wrapper(*args, **kwargs):
-        count_key = f"count:{args[0]}"
-        content_key = f"content:{args[0]}"
-
-        if r.exists(count_key):
-            r.incr(count_key)
-        else:
-            r.set(count_key, 1)
-
-        if r.exists(content_key):
-            html = r.get(content_key)
-        else:
-            html = func(*args, **kwargs)
-            r.set(content_key, html, ex=10)
-
-        return html
-    return wrapper
-
-@cache
-def get_page(url: str) -> str:
-    response = requests.get(url)
-    return response.text
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
