579e699fc3c9422e8298169163ad048abea2f190
baye94 <fallcheikh893@gmail.com>
2023-01-20 17:14:38 +0000

    fix bugs

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -24,3 +24,26 @@ def get_page(url: str) -> str:
         r.set(content_key, html, ex=10)
 
     return html
+def cache(func):
+    def wrapper(*args, **kwargs):
+        count_key = f"count:{args[0]}"
+        content_key = f"content:{args[0]}"
+
+        if r.exists(count_key):
+            r.incr(count_key)
+        else:
+            r.set(count_key, 1)
+
+        if r.exists(content_key):
+            html = r.get(content_key)
+        else:
+            html = func(*args, **kwargs)
+            r.set(content_key, html, ex=10)
+
+        return html
+    return wrapper
+
+@cache
+def get_page(url: str) -> str:
+    response = requests.get(url)
+    return response.text
